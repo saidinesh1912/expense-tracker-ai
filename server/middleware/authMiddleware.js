82f7e8825d84d65e4dcd90b5569b576a0cd2071e
saidinesh1912 <gavsaidinesh@gmail.com>
2026-05-24 17:31:55 +0530

    fixed jwt middleware bearer token

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,28 +2,70 @@ const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
 
-  const token = req.headers.authorization;
+  const authHeader =
+    req.headers.authorization;
 
-  if (!token) {
+  if (!authHeader) {
+
     return res.status(401).json({
-      message: "No token provided",
+
+      message:
+        "No token provided",
+
     });
+
   }
 
   try {
 
-    const decoded = jwt.verify(token, "secretkey");
+    // Remove Bearer
+    const token =
+      authHeader.startsWith(
+        "Bearer "
+      )
 
-    req.userId = decoded.id;
+      ? authHeader.split(
+          " "
+        )[1]
+
+      : authHeader;
+
+    const decoded =
+      jwt.verify(
+
+        token,
+
+        process.env.JWT_SECRET
+
+      );
+
+    req.userId =
+      decoded.id;
+
+    req.userEmail =
+      decoded.email;
 
     next();
 
-  } catch (error) {
-
-    res.status(401).json({
-      message: "Invalid token",
-    });
   }
+
+  catch(error){
+
+    console.log(
+      "JWT ERROR:",
+      error.message
+    );
+
+    return res.status(401).json({
+
+      message:
+        "Invalid token",
+
+    });
+
+  }
+
 };
 
-module.exports = authMiddleware;
+module.exports =
+  authMiddleware;
